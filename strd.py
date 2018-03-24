410fef95de13b1d2cc1c4eed7ad8f65c691ca2c3
Zongqian Zhang <1650658858@qq.com>
2018-03-24 09:17:19 +0800

    Update strd.py

--- strd.py
+++ strd.py
@@ -1,33 +1,25 @@
 import os
 import time
-import subprocess
-from subprocess import *
 
-sleep = time.sleep
+import threading
 
-def douyu(fullc):
-    pass
 
-def pandatv(fullc):
+def youd(c,m):
     while True:
         try:
-            subprocess.check_call(fullc)
-        except ValueError as e:
-            print('{},等待重试。。。'.format(e))
+            os.system('you-get www.{}.com/{} -o /root/b/d'.format(c,m))
+        except:
+            pass
         finally:
-            sleep(20)
-            
-            
-    print('测试成功{}'.format(fullc))
+            time.sleep(20)
+
 
 def main():
-    aname,aid = input('输入平台和id，空格隔开：').split(' ')
-    fullc='you-get www.{}.com/{} -o /root/b/d'.format(aname,aid)
-    if aname == 'pandatv':
-        pandatv(fullc)
-    elif aname =='douyu':
-        douyu(fullc)
-        
-        
+    ms = []
+    c,m=input('输入平台 room：').split(' ')
+    ms = m.split(',')
+    for room in ms:
+        down = threading.Thread(target=youd,args=(room,))
+        dowm.start()
 if __name__ =="__main__":
     main()
