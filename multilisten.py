77009274c131884121853bb03861606620718183
Zongqian Zhang <1650658858@qq.com>
2018-07-09 23:17:12 +0800

    Update multilisten.py

--- multilisten.py
+++ multilisten.py
@@ -213,15 +213,15 @@ class Room():
             n = 1024*1024;
             bBuffer = res.read(1024 * 128);
             tnumber = 0
-            print('剩余空间%.2f\n' % (available))
+            #print('剩余空间%.2f\n' % (available))
             while bBuffer:
                 nSize += f1.write(bBuffer);
                 if (nVerbose):
                     stream.write('\r{:<4.2f} MB downloaded'.format(nSize/n));
                 tnumber+=1               
-                if (tnumber>=1000):
+                if (tnumber>=200):
                     available=vfs.f_bavail*vfs.f_bsize/(1024*1024*1024)
-                    print('剩余空间%.2f\n' % (available))
+                    #print('剩余空间%.2f\n' % (available))
                     tnumber = 0
                 if (available<2):
                     break
