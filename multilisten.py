32cb156484beb88236c3318b18b27f33336c14a5
Zongqian Zhang <1650658858@qq.com>
2018-09-01 18:08:46 +0800

    Update multilisten.py

--- multilisten.py
+++ multilisten.py
@@ -57,7 +57,7 @@ def prepare():
     
     r = requests.get('http://127.0.0.1:8000/?types=0&count=5&country=国内')
     ip_ports = json.loads(r.text)
-    print ip_ports
+    print(ip_ports)
     ip = ip_ports[0][0]
     port = ip_ports[0][1]
     proxies={
