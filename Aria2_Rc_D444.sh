2851548c06ecc979d055e89fd5f99354dae5ccef
Zongqian Zhang <1650658858@qq.com>
2018-11-01 11:59:00 +0800

    Update Aria2_Rc_D444.sh

--- Aria2_Rc_D444.sh
+++ Aria2_Rc_D444.sh
@@ -339,13 +339,14 @@ git clone "https://github.com/Miloxing/aria2.git"
 
 cd "aria2/"
 
-apt-get install autoconf libtool automake autoconf-archive
-autoconf configure.ac
-autoconf -i
+apt-get update
+apt-get install libgnutls28-dev nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config libcppunit-dev autoconf automake autotools-dev autopoint libtool git gcc g++ libxml2-dev make quilt
+autoreconf -i
+#autoconf configure.ac
 ./configure
 make
 
-make install
+sudo make install
 cd /root
 rm -rf aria2 aria2-${aria2_new_ver}-linux-gnu-64bit-build1.tar.bz2
 mkdir "/root/.aria2" && cd "/root/.aria2"
