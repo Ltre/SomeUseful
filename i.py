d6f9220141e4e7dc41a51bc7e7a35a968470c912
Zongqian Zhang <1650658858@qq.com>
2018-11-15 01:42:16 +0800

    Add files via upload

---
+++ i.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+
+def main():
+    while True:
+        try:
+            os.system('cd /root/i && python3 I*')
+        except:
+            pass
+        finally:
+            time.sleep(2)
+        
+if __name__ =="__main__":
+    main()
