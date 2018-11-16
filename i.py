affd47e0e8101a87b9a22ae5033a58abd96f8580
Zongqian Zhang <1650658858@qq.com>
2018-11-16 20:41:21 +0800

    Update i.py

--- i.py
+++ i.py
@@ -3,6 +3,7 @@ import time
 
 
 def main():
+    os.system("apt-get install -y sqlite3 python3-lxml && pip3 install web.py==0.40-dev1 requests chardet sqlalchemy gevent psutil")
     while True:
         try:
             os.system('cd /root/i && python3 I*')
