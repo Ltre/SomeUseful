3aa0fc154b449c92a298e7850688cf7b3d18bdc2
Miloxing <1650658858@qq.com>
2019-08-16 16:15:19 +0800

    update

--- bdo.sh
+++ bdo.sh
@@ -17,71 +17,93 @@ do
 		echo "开始上传${f}至milo:milo/b/${arr[1]}"
 		#rclone move "${f}" "milo:milo/b/${arr[1]}" --bwlimit 10M
 		if [ ! -d "/home/milo/b/${arr[1]}" ]
-                then mkdir -p "/home/milo/b/${arr[1]}"
-                fi
-                #mv  "${f}" "/home/milo/b/${arr[1]}"
+		then mkdir -p "/home/milo/b/${arr[1]}"
+        fi
+        #mv  "${f}" "/home/milo/b/${arr[1]}"
 		rclone move "${f}" "milo:milo/b/${arr[1]}" --buffer-size 32M --transfers 4 -P --low-level-retries 1 
 		if [ -f $f ]
 		then if [ ! -d "/home/milo2/b/${arr[1]}" ]
-                	then mkdir -p "/home/milo2/b/${arr[1]}"
-                	fi
-                	#mv  "${f}" "/home/milo2/b/${arr[1]}"
-                	echo "开始上传${f}至milo2:milo/b/${arr[1]}"
+			then mkdir -p "/home/milo2/b/${arr[1]}"
+			fi
+			#mv  "${f}" "/home/milo2/b/${arr[1]}"
+			echo "开始上传${f}至milo2:milo/b/${arr[1]}"
 			rclone move "${f}" "milo2:milo/b/${arr[1]}" --buffer-size 32M --transfers 4 -P --low-level-retries 1
 			if [ -f $f ]
-                	then if [ ! -d "/home/milo3/b/${arr[1]}" ]
-                    	then mkdir -p "/home/milo3/b/${arr[1]}"
-                    	fi
-			#mv  "${f}" "/home/milo3/b/${arr[1]}"
-			echo "开始上传${f}至milo3:milo/b/${arr[1]}"
-			rclone move "${f}" "milo3:milo/b/${arr[1]}" --buffer-size 32M --transfers 4  -P --low-level-retries 1
-                    fi
+			then if [ ! -d "/home/milo3/b/${arr[1]}" ]
+				then mkdir -p "/home/milo3/b/${arr[1]}"
+				fi
+				#mv  "${f}" "/home/milo3/b/${arr[1]}"
+				echo "开始上传${f}至milo3:milo/b/${arr[1]}"
+				rclone move "${f}" "milo3:milo/b/${arr[1]}" --buffer-size 32M --transfers 4  -P --low-level-retries 1
+				if [ -f $f ]
+				then if [ ! -d "/home/milo4/b/${arr[1]}" ]
+					then mkdir -p "/home/milo4/b/${arr[1]}"
+					fi
+					echo "开始上传${f}至milo4:milo/b/${arr[1]}"
+					rclone move "${f}" "milo4:milo/b/${arr[1]}" --buffer-size 32M --transfers 4  -P --low-level-retries 1
+				fi
+			fi
 		fi
+		if [ ! -f $f ]
+		then
 			#rclone copy "${f}" "milo:milo/b/${arr[1]}" --bwlimit 10M
-		echo "${f} 上传完成"
-		#rm "${f}"
-		
+			echo "${f} 上传完成"
+			#rm "${f}"
+		else
+			echo "${f} 未上传"
+		fi
 	else
 		sleep 1
 	fi
 done
 for f in *.mp4
 do
-        if [ ${f} != "*.mp4" ]
-        then
-                OLD_IFS="$IFS"
-                IFS="-"
-                arr=($f)
-                IFS="$OLD_IFS"
+	if [ ${f} != "*.mp4" ]
+	then
+		OLD_IFS="$IFS"
+		IFS="-"
+		arr=($f)
+		IFS="$OLD_IFS"
 		#rclone move "${f}" "milo:milo/b/huya/${arr[1]}" --bwlimit 10M
 		if [ ! -d "/home/milo/b/huya/${arr[1]}" ]
-                then mkdir -p "/home/milo/b/huya/${arr[1]}"
-                fi
-                #mv  "${f}" "/home/milo/b/huya/${arr[1]}"
+		then mkdir -p "/home/milo/b/huya/${arr[1]}"
+		fi
+		#mv  "${f}" "/home/milo/b/huya/${arr[1]}"
 		echo "开始上传${f}至milo:milo/b/huya/${arr[1]}"
 		rclone move "${f}" "milo:milo/b/huya/${arr[1]}" --buffer-size 32M --transfers 4 -P --low-level-retries 1
 		if [ -f $f ]
 		then if [ ! -d "/home/milo2/b/huya/${arr[1]}" ]
-                	then mkdir -p "/home/milo2/b/huya/${arr[1]}"
-                	fi
-                	#mv  "${f}" "/home/milo2/b/huya/${arr[1]}"
-                	echo "开始上传${f}至milo2:milo/b/${arr[1]}"
+			then mkdir -p "/home/milo2/b/huya/${arr[1]}"
+			fi
+			#mv  "${f}" "/home/milo2/b/huya/${arr[1]}"
+			echo "开始上传${f}至milo2:milo/b/${arr[1]}"
 			rclone move "${f}" "milo2:milo/b/huya/${arr[1]}" --buffer-size 32M --transfers 4 -P --low-level-retries 1
 			if [ -f $f ]
-                	then if [ ! -d "/home/milo3/b/huya/${arr[1]}" ]
-                    	then mkdir -p "/home/milo3/b/huya/${arr[1]}"
-                    	fi
-                    	#mv  "${f}" "/home/milo3/b/huya/${arr[1]}"
-			echo "开始上传${f}至milo3:milo/b/${arr[1]}"
-			rclone move "${f}" "milo3:milo/b/huya/${arr[1]}" --buffer-size 32M --transfers 4 -P --low-level-retries 1
-                    fi
+			then if [ ! -d "/home/milo3/b/huya/${arr[1]}" ]
+				then mkdir -p "/home/milo3/b/huya/${arr[1]}"
+				fi
+				#mv  "${f}" "/home/milo3/b/huya/${arr[1]}"
+				echo "开始上传${f}至milo3:milo/b/${arr[1]}"
+				rclone move "${f}" "milo3:milo/b/huya/${arr[1]}" --buffer-size 32M --transfers 4 -P --low-level-retries 1
+				if [ -f $f ]
+				then if [ ! -d "/home/milo4/b/huya/${arr[1]}" ]
+					then mkdir -p "/home/milo4/b/huya/${arr[1]}"
+					fi
+					echo "开始上传${f}至milo4:milo/b/${arr[1]}"
+					rclone move "${f}" "milo4:milo/b/huya/${arr[1]}" --buffer-size 32M --transfers 4 -P --low-level-retries 1
+				fi
+			fi
 		fi
-                echo "${f}上传成功"
-                #rm "${f}"
-        else
-                sleep 1
-        fi
+		if [ ! -f $f ]
+		then
+			echo "${f}上传成功"
+			#rm "${f}"
+		else
+			echo "${f} 未上传"
+		fi
+	else
+		sleep 1
+	fi
 done
-
 sleep 5
 done
